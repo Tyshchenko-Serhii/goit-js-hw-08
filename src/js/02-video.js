c93946d32d2da8c2f69e6e831c4e682ba0c61677
Tars-El <106482481+Tars-El@users.noreply.github.com>
2022-12-16 00:35:31 +0200

    Second HW done.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = function (data) {
+  const playedTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', playedTime);
+};
+
+const savePletedTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+if (savePletedTime) {
+  player.setCurrentTime(savePletedTime);
+}
